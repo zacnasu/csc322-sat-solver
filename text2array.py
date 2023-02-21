80c6aebac94ebb3a4012fea24ef2fee29e8ddfdd
brockchad <76860502+brockchad@users.noreply.github.com>
2023-02-20 21:14:19 -0800

    Update text2array.py

--- text2array.py
+++ text2array.py
@@ -1,13 +1,15 @@
 import sys
 
 # Read puzzle from stdin
-puzzle_str = sys.stdin.readline().strip()
+#0:81 is the index first 9 x 9 puzzle from the
+puzzle_str = ''.join((sys.stdin.read().split('\n')))[0:81]
 
-# Convert puzzle string to a 2D array
-puzzle = [[int(puzzle_str[i + j*9]) if puzzle_str[i + j*9].isdigit() else 0 for i in range(9)] for j in range(9)]
+size = 9
+
+#Convert puzzle string to a 2D array
+puzzle = [[int(puzzle_str[i + j * size]) if puzzle_str[i + j * size].isdigit() else 0 for i in range(size)] for j in
+          range(size)]
 
 # Print the puzzle as a 2D array
 #for row in puzzle:
     #print(row)
-
-#print(puzzle)
